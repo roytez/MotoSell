09ce4c5a665c26e41386d5423792d4c12ddca674
roytez <mateuszkrasinkiewicz@gmail.com>
2020-08-13 13:46:12 +0200

    change basic settings

--- MotoSell/motosell/settings.py
+++ MotoSell/motosell/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aji9#m4_v71!v*37!u9e079rohzd#^=tx@hz9gs#j+^-%sw2-k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
